557e978a35c5bdf3ffe6e3dd60631744e95e3cf6
wyslan alves <wyslandasilvaalves@gmail.com>
2022-10-17 22:37:43 -0300

    criação categoria

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -14,7 +14,6 @@ import javax.persistence.Table;
 
 import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity
 @Table(name = "tb_order")
